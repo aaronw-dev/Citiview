40c613c81cf8abe3796c71b64ce845894c4cea8d
IngeniousCoders <92069679+IngeniousCoders@users.noreply.github.com>
2024-08-24 06:48:32 -0700

    Fixed animations

--- resources/js/landing_page.js
+++ resources/js/landing_page.js
@@ -2,12 +2,27 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         console.log(entry);
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('show_1');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('show_1');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hidden_1');
 hiddenElements.forEach((el) => observer.observe(el))
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show_2');
+        } else {
+            entry.target.classList.remove('show_2');
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden_2');
+hiddenElements2.forEach((el) => observer.observe(el))
+
